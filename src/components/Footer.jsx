6958c0e5e9881156911a10842ab6d4ed912381dc
Michael M <makin99@yandex.ru>
2022-08-25 17:39:11 +0500

    isolated link hover into class

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer(props) {
         <h3>Designed by me</h3>
       </div>
       <div className="footer-contacts">
-        <a href='tel:88005553535' className="footer-contact__links">tel: 8-800-555-35-35</a>
-        <a href="mailto:auf@gmail.com" className="footer-contact__links">mail: auf@gmail.com</a>
+        <a href='tel:88005553535' className="link-hover">tel: 8-800-555-35-35</a>
+        <a href="mailto:auf@gmail.com" className="link-hover">mail: auf@gmail.com</a>
       </div>
     </footer>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,15 @@ export default function Header(props) {
   return (
     <header className="header">
       <nav className="header-nav container">
-        <Link to="/" className="header-navigation header-navigation__logo">
+        <Link to="/" className="header-navigation header-navigation__logo link-hover">
           <img src={logo} alt="logo" height={55}/>
         </Link>
         <div>
-          <NavLink to="/about" className="header-navigation" style={({isActive}) => isActive ? activeStyle : undefined}>
+          <NavLink to="/about" className="header-navigation link-hover"
+                   style={({isActive}) => isActive ? activeStyle : undefined}>
             About
           </NavLink>
-          <NavLink to="/contacts" className="header-navigation"
+          <NavLink to="/contacts" className="header-navigation link-hover"
                    style={({isActive}) => isActive ? activeStyle : undefined}>
             Contacts
           </NavLink>
